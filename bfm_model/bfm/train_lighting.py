86622846b249232f30f227d43b9db9edb8210383
Nexardus <seba270270270@gmail.com>
2025-05-25 21:22:16 +0200

    Added possibility of configuring customary backbone sizes

--- bfm_model/bfm/train_lighting.py
+++ bfm_model/bfm/train_lighting.py
@@ -7,7 +7,7 @@ import os
 import pickle
 from datetime import datetime, timedelta
 from functools import partial
-from typing import Literal
+from typing import Literal, Tuple
 
 import hydra
 import lightning as L
@@ -131,6 +131,17 @@ class BFM_lighting(LightningModule):
         land_mask_path: str = "",
         use_mask: str = "no",
         partially_masked_groups: list[str] = ["species_variables"],
+        swin_encoder_depths: Tuple[int, ...] = (2,2,2),
+        swin_encoder_num_heads: Tuple[int, ...] = (8,16,32),
+        swin_decoder_depths: Tuple[int, ...] = (2,2,2),
+        swin_decoder_num_heads: Tuple[int, ...] = (32,16,8),
+        swin_window_size: Tuple[int, ...] = (1,4,5),
+        swin_mlp_ratio: float = 4.0,
+        swin_qkv_bias: bool = True,
+        swin_drop_rate: float = 0.0,
+        swin_attn_drop_rate: float = 0.0,
+        swin_drop_path_rate: float = 0.1,
+        swin_use_lora: bool = False,
         **kwargs,
     ):
         super().__init__()
@@ -145,6 +156,18 @@ class BFM_lighting(LightningModule):
         self.td_learning = td_learning
         self.use_mask = use_mask
         self.partially_masked_groups = partially_masked_groups
+
+        self.swin_encoder_depths = swin_encoder_depths
+        self.swin_encoder_num_heads = swin_encoder_num_heads
+        self.swin_decoder_depths = swin_decoder_depths
+        self.swin_decoder_num_heads = swin_decoder_num_heads
+        self.swin_window_size = swin_window_size
+        self.swin_mlp_ratio = swin_mlp_ratio
+        self.swin_qkv_bias = swin_qkv_bias
+        self.swin_drop_rate = swin_drop_rate
+        self.swin_attn_drop_rate = swin_attn_drop_rate
+        self.swin_drop_path_rate = swin_drop_path_rate
+        self.swin_use_lora = swin_use_lora
 
         # load land-sea mask
         try:
@@ -211,17 +234,17 @@ class BFM_lighting(LightningModule):
         if backbone_type == "swin":
             self.backbone = Swin3DTransformer(
                 embed_dim=embed_dim,
-                encoder_depths=(2, 2),
-                encoder_num_heads=(8, 16),
-                decoder_depths=(2, 2),
-                decoder_num_heads=(32, 16),
-                window_size=(1, 1, 1), # ()
-                mlp_ratio=4.0,
-                qkv_bias=True,
-                drop_rate=0.0,
-                attn_drop_rate=0.0,
-                drop_path_rate=0.1,
-                use_lora=False,
+                encoder_depths=self.swin_encoder_depths,
+                encoder_num_heads=self.swin_encoder_num_heads,
+                decoder_depths=self.swin_decoder_depths,
+                decoder_num_heads=self.swin_decoder_num_heads,
+                window_size=self.swin_window_size,
+                mlp_ratio=self.swin_mlp_ratio,
+                qkv_bias=self.swin_qkv_bias,
+                drop_rate=self.swin_drop_rate,
+                attn_drop_rate=self.swin_attn_drop_rate,
+                drop_path_rate=self.swin_drop_path_rate,
+                use_lora=self.swin_use_lora,
             )
         elif backbone_type == "mvit":
             self.backbone = MViT(
@@ -568,6 +591,24 @@ def main(cfg):
         )
 
     print("Done \n Setting up the BFM")
+    
+    swin_params = {}
+    if cfg.model.backbone == "swin":
+        selected_swin_config = cfg.model_swin_backbone[cfg.model.swin_backbone_size]
+        swin_params = {
+            "swin_encoder_depths": tuple(selected_swin_config.encoder_depths),
+            "swin_encoder_num_heads": tuple(selected_swin_config.encoder_num_heads),
+            "swin_decoder_depths": tuple(selected_swin_config.decoder_depths),
+            "swin_decoder_num_heads": tuple(selected_swin_config.decoder_num_heads),
+            "swin_window_size": tuple(selected_swin_config.window_size),
+            "swin_mlp_ratio": selected_swin_config.mlp_ratio,
+            "swin_qkv_bias": selected_swin_config.qkv_bias,
+            "swin_drop_rate": selected_swin_config.drop_rate,
+            "swin_attn_drop_rate": selected_swin_config.attn_drop_rate,
+            "swin_drop_path_rate": selected_swin_config.drop_path_rate,
+            "swin_use_lora": selected_swin_config.use_lora,
+        }
+
     BFM = BFM_lighting(
         surface_vars=(cfg.model.surface_vars),
         edaphic_vars=(cfg.model.edaphic_vars),
@@ -598,6 +639,7 @@ def main(cfg):
         land_mask_path=cfg.data.land_mask_path,
         use_mask=cfg.training.use_mask,
         partially_masked_groups=cfg.training.partially_masked_groups,
+        **swin_params,
     )
 
     apply_activation_checkpointing(BFM, checkpoint_wrapper_fn=checkpoint_wrapper, check_fn=activation_ckpt_policy)
